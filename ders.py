a3c5553144e741a4367ece713900d8550f418f96
Zehra Bekar <zehrabekar54@gmail.com>
2023-04-07 14:31:24 +0300

    first notes about python

---
+++ ders.py
@@ -0,0 +1,27 @@
+"""
+yorumlama = interprete
+yorumlayıcı = interpreter
+derleme = compile
+derleyici = compiler
+
+derleme işlemi bir kere yapar,yorumlama işlemleri tekrar tekrar yapar .
+derleme işlemi daha hızlı çalışır
+yorumlama işleminde hangi satırdaysak o satırın komutu belleğe alınır,derleyicide tüm komut belleğe alınır. bu yüzden yorumlayıcıda daha az belleğe ihtiyaç vardır.
+dosya uzantısı : .py
+"""
+
+#ekrana yazdırma komutu = print. örnek : 
+print("merhaba")
+#başka örnek :
+a=5
+b=10
+c=a+b
+print(c) #15 yazdırdı
+
+"""
+\t : tab tuşu kadar boşluk bırakır
+\n : enter'a karşılık gelir. print ile yazdırdığımız ifadeden sonra \n nedeniyle imleç alt satıra geçer
+"""
+
+# çok satırlı yorum satırı yazmak için -> """  """  ya da '''  ''' kullanılır
+# tek satır yorum yazmak için # kullanılır
